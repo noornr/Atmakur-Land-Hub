517887bb00d136cdcb54c567e61fe6e2f096a472
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-05 23:13:52 +0530

    Refactor properties from array to object structure

--- properties.js
+++ properties.js
@@ -1,31 +1,69 @@
+const properties = {
 
-const properties = [
+"ATK-1":{
 
-{
 id:"ATK-1",
-title:"Minority Colony",
+
 price:"₹7,50,000",
+
+area:"Minority Colony",
+
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"1.5 Cents",
+
+landTelugu:"1.5 సెంట్లు",
+
+road:"30 Feet Road",
+
+roadTelugu:"30 అడుగులు రోడ్డు",
+
+description:"A premium open plot.",
+
 status:"AVAILABLE",
-image:"images/property1.jpg",
-page:"property.html?id=ATK-1"
+
+images:[
+"images/property1.jpg",
+"images/property1-2.jpg",
+"images/property1-3.jpg",
+"images/property1-4.jpg"
+],
+
+map:"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d583.0667482873671!2d78.61291267106094!3d15.879715429108323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1784827657882!5m2!1sen!2sin"
+
 },
 
-{
+"ATK-2":{
+
 id:"ATK-2",
-title:"Srisailam Road",
-price:"₹18,00,000",
-status:"AVAILABLE",
-image:"images/property2.jpg",
-page:"property.html?id=ATK-2"
-},
 
-{
-id:"ATK-3",
-title:"Property 3",
-price:"₹10,00,000",
-status:"AVAILABLE",
-image:"images/property3.jpg",
-page:"property.html?id=ATK-3"
+price:"1 Cent ₹4,50,000",
+
+area:"Nandyala Road Highway Side",
+
+areaTelugu:"నంద్యాల రోడ్డు హైవే పక్కన",
+
+land:"4 Cents",
+
+landTelugu:"4 సెంట్లు",
+
+road:"30 Feet Road",
+
+roadTelugu:"30 అడుగులు రోడ్డు",
+
+description:"Highway facing premium plot.",
+
+status:"SOLD",
+
+images:[
+"images/property2.jpg",
+"images/property2-2.jpg",
+"images/property2-3.jpg",
+"images/property2-4.jpg"
+],
+
+map:"YOUR_MAP_LINK"
+
 }
 
-];
+};

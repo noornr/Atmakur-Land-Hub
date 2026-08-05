ef924bbd2f6926ef245867f8fa1d2bbdfbb498f2
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-05 23:01:36 +0530

    Add property listings to properties.js

--- properties.js
+++ properties.js
@@ -1,1 +1,31 @@
 
+const properties = [
+
+{
+id:"ATK-1",
+title:"Minority Colony",
+price:"₹7,50,000",
+status:"AVAILABLE",
+image:"images/property1.jpg",
+page:"property.html?id=ATK-1"
+},
+
+{
+id:"ATK-2",
+title:"Srisailam Road",
+price:"₹18,00,000",
+status:"AVAILABLE",
+image:"images/property2.jpg",
+page:"property.html?id=ATK-2"
+},
+
+{
+id:"ATK-3",
+title:"Property 3",
+price:"₹10,00,000",
+status:"AVAILABLE",
+image:"images/property3.jpg",
+page:"property.html?id=ATK-3"
+}
+
+];

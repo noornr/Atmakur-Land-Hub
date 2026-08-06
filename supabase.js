118e96cf0d54d7ed557c29855a32e627a475da3b
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-06 13:30:50 +0530

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://soltusisytegacqbjhsh.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_stQnnktiByN2OEGK2bURWQ_W3TQb8B_";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);

0939a42d2b41a5d2a328e0708fd1203a6c7c767d
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-09 11:59:20 +0530

    Add new property entries to properties.js
    Added multiple property entries with details including price, area, facing, and images.

--- properties.js
+++ properties.js
@@ -205,4 +205,597 @@ map:"YOUR_GOOGLE_MAP_LINK"
 
 },
 
+  "ATK-7":{
+
+id:"ATK-7",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property7.jpg",
+"images/property7-2.jpg",
+"images/property7-3.jpg",
+"images/property7-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-8":{
+
+id:"ATK-8",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property8.jpg",
+"images/property8-2.jpg",
+"images/property8-3.jpg",
+"images/property8-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-9":{
+
+id:"ATK-9",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property9.jpg",
+"images/property9-2.jpg",
+"images/property9-3.jpg",
+"images/property9-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-10":{
+
+id:"ATK-10",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property10.jpg",
+"images/property10-2.jpg",
+"images/property10-3.jpg",
+"images/property10-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-11":{
+
+id:"ATK-11",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property11.jpg",
+"images/property11-2.jpg",
+"images/property11-3.jpg",
+"images/property11-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-12":{
+
+id:"ATK-12",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property12.jpg",
+"images/property12-2.jpg",
+"images/property12-3.jpg",
+"images/property12-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+  "ATK-13":{
+
+id:"ATK-13",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property13.jpg",
+"images/property13-2.jpg",
+"images/property13-3.jpg",
+"images/property13-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-14":{
+
+id:"ATK-14",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property14.jpg",
+"images/property14-2.jpg",
+"images/property14-3.jpg",
+"images/property14-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-15":{
+
+id:"ATK-15",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property15.jpg",
+"images/property15-2.jpg",
+"images/property15-3.jpg",
+"images/property15-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-16":{
+
+id:"ATK-16",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property16.jpg",
+"images/property16-2.jpg",
+"images/property16-3.jpg",
+"images/property16-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-17":{
+
+id:"ATK-17",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property17.jpg",
+"images/property17-2.jpg",
+"images/property17-3.jpg",
+"images/property17-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-18":{
+
+id:"ATK-18",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property18.jpg",
+"images/property18-2.jpg",
+"images/property18-3.jpg",
+"images/property18-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-19":{
+
+id:"ATK-19",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property19.jpg",
+"images/property19-2.jpg",
+"images/property19-3.jpg",
+"images/property19-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-20":{
+
+id:"ATK-20",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property20.jpg",
+"images/property20-2.jpg",
+"images/property20-3.jpg",
+"images/property20-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-21":{
+
+id:"ATK-21",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property21.jpg",
+"images/property21-2.jpg",
+"images/property21-3.jpg",
+"images/property21-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-22":{
+
+id:"ATK-22",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property22.jpg",
+"images/property22-2.jpg",
+"images/property22-3.jpg",
+"images/property22-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
+
+"ATK-23":{
+
+id:"ATK-23",
+
+status:"AVAILABLE",
+
+price:"₹7,50,000",
+
+area:"Minority Colony",
+areaTelugu:"మైనారిటీ కాలనీ",
+
+land:"8 Cents",
+landTelugu:"8 సెంట్లు",
+
+facing:"East Face",
+facingTelugu:"తూర్పు ముఖం",
+
+road:"30 Feet Road",
+roadTelugu:"30 అడుగుల రోడ్డు",
+
+description:"A premium open plot.",
+descriptionTelugu:"అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+images:[
+"images/property23.jpg",
+"images/property23-2.jpg",
+"images/property23-3.jpg",
+"images/property23-4.jpg"
+],
+
+map:"YOUR_GOOGLE_MAP_LINK"
+
+},
+
 };

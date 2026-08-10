bf748fc2a5e54b7677ea9480e98dd598955a9bce
OMG_X_NOOR <53120072+noornr@users.noreply.github.com>
2026-08-10 11:54:16 +0530

    Add new property 'ATK-25' with details

--- properties.js
+++ properties.js
@@ -833,4 +833,42 @@ map:"YOUR_GOOGLE_MAP_LINK"
 
 },
 
+  "ATK-25": {
+
+    id: "ATK-25",
+
+    featured: true,
+
+    status: "AVAILABLE",
+
+    price: "₹7,50,000",
+
+    area: "Minority Colony",
+    areaTelugu: "మైనారిటీ కాలనీ",
+
+    land: "8 Cents",
+    landTelugu: "8 సెంట్లు",
+
+    facing: "East Face",
+    facingTelugu: "తూర్పు ముఖం",
+
+    road: "30 Feet Road",
+    roadTelugu: "30 అడుగుల రోడ్డు",
+
+    description: "A premium open plot.",
+    descriptionTelugu: "అత్యుత్తమ ఓపెన్ ప్లాట్.",
+
+    images: [
+        "images/property25.jpg",
+        "images/property25-2.jpg",
+        "images/property25-3.jpg",
+        "images/property25-4.jpg"
+    ],
+
+    map: "YOUR_GOOGLE_MAP_LINK"
+
+}
+
+};
+
 };
